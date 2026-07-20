cb4110ea4be4c7f0e2cae2f34fac2f9b933a1072
palak11220-coder <palak11220@gmail.com>
2026-07-21 00:31:38 +0530

    Dictionary Methods

---
+++ My_firstRepo/Day34.py
@@ -0,0 +1,10 @@
+ep1 = {122: 45, 123: 80, 567: 89, 670: 69}
+ep2 = {222: 67, 566: 90}
+#ep1.update(ep2) #this will upload ep2 in ep1 
+#ep1.clear() this clear the ep1
+#ep1.pop(122) this will pop that item from a dictionary
+#ep1.popitem() removes the last keypair
+#del ep1 deletes the whole dictionary pass the key to del that particular key value pair
+
+
+print(ep1)
